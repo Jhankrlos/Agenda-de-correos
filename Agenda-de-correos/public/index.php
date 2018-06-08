b96e6dd3c81ae34fba41b6059f4ec371a749e519
Jhan Carlos Lucio Ríos <jhankrloslucio@gmail.com>
2018-06-07 21:29:07 -0500

    Programación 2/4
    Se agregan unos archivos de código

---
+++ Agenda-de-correos/public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+        <link rel="stylesheet" href="css/styleindex.css">
+    </head>
+    <body>
+        <header>
+            <div class="width">
+                <div class="logo">
+                    <img class="logophoto" src="img/agenda.png">
+                    <p>Agendamail.com</p> 
+                    
+                </div>
+            </div>
+        </header>
+        
+    </body>
+</html>
+
